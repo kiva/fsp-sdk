7d507778b652c290170d7236bb4ade8561f2b489
Matthew Raffel <mattr@kiva.org>
2022-11-15 12:52:34 -0700

    switching to fetch
    Signed-off-by: Matthew Raffel <mattr@kiva.org>

--- samples/typescript/src/demo/auth.ts
+++ samples/typescript/src/demo/auth.ts
@@ -1,48 +1,49 @@
-import axios from 'axios';
 import * as dotenv from 'dotenv';
-const querystring = require('querystring');
-
-class AuthorizationResponse {
-    public access_token: string = '';
-    public token_type: string = '';
-    public expires_in: number = 0;
-    public scope: string = '';
-    public iss: string = '';
-    public partnerId: string = '';
-    public jti: string = '';
-}
 
 const log = (text: string, data?: any) => {
     console.log(text, data || '');
 };
 
-// https://stackoverflow.com/questions/31756756/axios-http-client-how-to-construct-http-post-url-with-form-params
 const gethAuthToken = async () => {
     dotenv.config();
     log(`getting autho token using client_id '${process.env.client_id}'`);
     log(`\twith secret '${process.env.client_secret}'`);
 
-
-    const inputData = querystring.stringify({
+    const uri: string = 'https://auth-stage.dk1.kiva.org/oauth/token';
+    const details = {
         grant_type: 'client_credentials',
         scope: 'create:loan_draft read:loans',
         audience: 'https://partner-api-stage.dk1.kiva.org',
         client_id: process.env.client_id,
         client_secret: process.env.client_secret,
-    });
-
-    const config = {
-        headers:  {
-            Accept: 'application/json',
-            'Content-Type': 'application/x-www-form-urlencoded',
-        }
     };
 
+    const headers =  {
+            Accept: 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded',
+        };
+
+    var formBody = [];
+    for (var property in details) {
+        var encodedKey = encodeURIComponent(property);
+        // @ts-ignore
+        var encodedValue = encodeURIComponent(details[property]);
+        formBody.push(encodedKey + "=" + encodedValue);
+    }
+    const body = formBody.join("&");
+
     try {
-        const { data, status } = await axios.post<AuthorizationResponse>('https://auth-stage.dk1.kiva.org/oauth/token', {
-            inputData,
-            config,
+        const response = await fetch(uri, {
+            method: 'POST',
+            headers,
+            body
         });
+
+        const data = await response.json();
+        log(`access_token:`, data.access_token);
+        log(`token_type:`, data.token_type);
+        log(`partnerId`, data.partnerId);
+
     } catch (e) {
         if (e instanceof Error) {
             log(`error: ${e.message}`);
