f930e1e5cd2669c247282cc79e423d2745f9bc00
Matthew Raffel <mattr@kiva.org>
2022-11-15 15:36:24 -0700

    documentation improvements
    Signed-off-by: Matthew Raffel <mattr@kiva.org>

--- samples/typescript/src/demo/get.loans.ts
+++ samples/typescript/src/demo/get.loans.ts
@@ -4,8 +4,8 @@ const log = (text: string, data?: any) => {
     console.log(text, data || '');
 };
 
-let partnerId: string = '';
-let authToken: string = '';
+let partnerId = '';
+let authToken = '';
 
 // this method is the same as in the auth example
 // please see that example for more comments
@@ -14,7 +14,7 @@ const gethAuthToken = async () => {
     log(`getting autho token using client_id '${process.env.client_id}'`);
     log(`\twith secret '${process.env.client_secret}'`);
 
-    const uri: string = 'https://auth-stage.dk1.kiva.org/oauth/token';
+    const uri = 'https://auth-stage.dk1.kiva.org/oauth/token';
     const details = {
         grant_type: 'client_credentials',
         scope: 'create:loan_draft read:loans',
@@ -23,29 +23,28 @@ const gethAuthToken = async () => {
         client_secret: process.env.client_secret,
     };
 
-    const headers =  {
-            Accept: 'application/json',
-            'Content-Type': 'application/x-www-form-urlencoded',
-        };
+    const headers = {
+        Accept: 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded',
+    };
 
-    var formBody = [];
-    for (var property in details) {
-        var encodedKey = encodeURIComponent(property);
+    const formBody = [];
+    for (const property in details) {
+        const encodedKey = encodeURIComponent(property);
         // @ts-ignore
-        var encodedValue = encodeURIComponent(details[property]);
-        formBody.push(encodedKey + "=" + encodedValue);
+        const encodedValue = encodeURIComponent(details[property]);
+        formBody.push(encodedKey + '=' + encodedValue);
     }
-    const body = formBody.join("&");
+    const body = formBody.join('&');
     const response = await fetch(uri, {
         method: 'POST',
         headers,
-        body
+        body,
     });
 
     const data = await response.json();
     partnerId = data.partnerId;
     authToken = data.access_token;
-
 };
 
 const getLoans = async () => {
@@ -54,15 +53,15 @@ const getLoans = async () => {
     // get the auth token since that will be needed for the next call
     await gethAuthToken();
 
-    const uri: string = `https://partner-api-stage.dk1.kiva.org/v3/partner/${partnerId}/loans`;
+    const uri = `https://partner-api-stage.dk1.kiva.org/v3/partner/${partnerId}/loans`;
     log(`calling ${uri}`);
 
     // creating the headers.  authToken value is retrieved from the function
     // gethAuthToken above.  authToken ensures you have permissions and rights to make
     // the api call below
-    const headers =  {
+    const headers = {
         Accept: 'application/json',
-        Authorization: `Bearer ${authToken}`
+        Authorization: `Bearer ${authToken}`,
     };
 
     // now make the get loans call sending only required fields and leaving all optional
@@ -71,10 +70,10 @@ const getLoans = async () => {
     // https://kivapartnerhelpcenter.zendesk.com/hc/en-us/articles/360051230151-Using-the-API-loans-fetch-endpoint
     const response = await fetch(uri, {
         method: 'GET',
-        headers
+        headers,
     });
 
-
+    // the data needs to be read from the stream
     const data = await response.json();
 
     // print out the results of the call.  if no loans are posted
@@ -82,14 +81,18 @@ const getLoans = async () => {
     log(`v3/loans received`, data);
 };
 
-
-getLoans().then(() => {
-    log(`GET loans api call completed successfully`);
-}).catch((e: unknown) =>{
-    if (e instanceof Error) {
-        log(`error: ${e.message}`);
-    } else {
-        log(`error, but not understood`, e);
-    }
-});
-
+// this code will get executed by running the command
+// npm run get-loans from the typescript directory
+getLoans()
+    .then(() => {
+        // this line executes if the api calls succeed
+        log(`GET loans api call completed successfully`);
+    })
+    .catch((e: unknown) => {
+        // errors will be output to the console here
+        if (e instanceof Error) {
+            log(`error: ${e.message}`);
+        } else {
+            log(`error, but not understood`, e);
+        }
+    });
